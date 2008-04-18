fee52e6c9c3bd90d4bb7352fd15fd4d9a53f170c
Justin Bogner <mail@justinbogner.com>
2008-04-17 23:02:19 -0600

    more robust runhaskell-setup

--- runhaskell-setup.sh
+++ runhaskell-setup.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 # a trivial script, because i do this a bunch
 
-runhaskell Setup.lhs configure --user --prefix=$HOME/local
-runhaskell Setup.lhs build
-runhaskell Setup.lhs install --user
+function die () {
+    echo $1
+    exit
+}
+
+if [ -e Setup.lhs ]; then
+    setup=Setup.lhs
+elif [ -e Setup.hs ]; then
+    setup=Setup.hs
+else
+    die "No suitable setup file found"
+fi
+
+[ "$#" -gt 0 ] && [ "$1" = "clean" ] && runhaskell $setup clean
+
+runhaskell $setup configure --user --prefix=$HOME/local &&
+runhaskell $setup build &&
+runhaskell $setup install --user
