eb9545f1fc5346a3f66ddcbe214eef57820a2a8f
Justin Bogner <mail@justinbogner.com>
2010-01-21 15:02:37 -0700

    break git-log up into some objects, so that it's testable

--- git-dot.py
+++ git-dot.py
@@ -1,50 +1,77 @@
 #!/usr/bin/env python
-import csv, sys
+import csv, sys, unittest
 from subprocess import Popen, PIPE
 
 ASCII_UNIT_SEPARATOR = 0x1f
+FIELD_SEPARATOR = ASCII_UNIT_SEPARATOR
 
 def main(argv):
-    outstream = sys.stdout
+    gitLog = GitLog(argv[1:])
+    GraphGenerator(sys.stdout, gitLog).generate()
 
-    outstream.write("digraph G {\n")
+class GitLog:
+    def __init__(self, args):
+        self.args = args
 
-    sep = '%x' % ASCII_UNIT_SEPARATOR
-    logFormat = ('%x' + sep).join(['%h', '%p', '%d'])
-    proc = Popen(['git', 'log', '--pretty=format:%s' % logFormat] + argv[1:],
-                 stdout = PIPE)
-    reader = csv.reader(proc.stdout, delimiter = chr(ASCII_UNIT_SEPARATOR))
-    for sha1, parents, refnames in reader:
-        edges = ['"%s"->"%s"' % (parent, sha1) for parent in parents.split()]
+    def getLogStream(self, format):
+        command = ['git', 'log', '--pretty=format:%s' % format] + self.args
+        proc = Popen(command, stdout = PIPE)
+        return proc.stdout
 
-        label = None
-        refnames = refnames.strip()
-        if refnames:
-            if refnames[0] == '(':
-                refnames = refnames[1:]
-            if refnames[-1] == ')':
-                refnames = refnames[:-1]
+class GraphGenerator:
+    def __init__(self, outputStream, gitLog):
+        self.output = outputStream
+        self.gitLog = gitLog
 
-            names = refnames.split(', ')
-            if len(names) == 1:
-                label = '/'.join(names[0].split('/')[2:])
-            elif len(names) > 1:
-                namesByKind = {}
-                for name in names:
-                    kind = name.split('/')[1]
-                    realname = '/'.join(name.split('/')[2:])
-                    namesByKind[kind] = realname
-                keys = namesByKind.keys()
-                for kind in ('tags', 'heads', 'remotes'):
-                    if kind in keys:
-                        label = namesByKind[kind]
-                        break
-        if not label:
-            label = sha1
-        line = '"%s" [label="%s"];%s;\n' % (sha1, label, ';'.join(edges))
-        outstream.write(line)
+    def generate(self):
+        self._writeHeader()
+        self._writeBody()
+        self._writeFooter()
 
-    outstream.write("}\n")
+    def _writeHeader(self):
+        self._writeLine('digraph G {')
+
+    def _writeLine(self, line):
+        self.output.write(line + '\n')
+
+    def _writeBody(self):
+        sep = '%x' % FIELD_SEPARATOR
+        logFormat = ('%x' + sep).join(['%h', '%p', '%d'])
+        stream = self.gitLog.getLogStream(logFormat)
+        reader = csv.reader(stream, delimiter = chr(FIELD_SEPARATOR))
+        for sha1, parents, refnames in reader:
+            edges = ['"%s"->"%s"' % (parent, sha1)
+                     for parent in parents.split()]
+
+            label = None
+            refnames = refnames.strip()
+            if refnames:
+                if refnames[0] == '(':
+                    refnames = refnames[1:]
+                if refnames[-1] == ')':
+                    refnames = refnames[:-1]
+
+                names = refnames.split(', ')
+                if len(names) == 1:
+                    label = '/'.join(names[0].split('/')[2:])
+                elif len(names) > 1:
+                    namesByKind = {}
+                    for name in names:
+                        kind = name.split('/')[1]
+                        realname = '/'.join(name.split('/')[2:])
+                        namesByKind[kind] = realname
+                    keys = namesByKind.keys()
+                    for kind in ('tags', 'heads', 'remotes'):
+                        if kind in keys:
+                            label = namesByKind[kind]
+                            break
+            if not label:
+                label = sha1
+            line = '"%s" [label="%s"];%s;' % (sha1, label, ';'.join(edges))
+            self._writeLine(line)
+
+    def _writeFooter(self):
+        self._writeLine('}')
 
 if __name__ == '__main__':
     main(sys.argv)
