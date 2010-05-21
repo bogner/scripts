00aae52ced1880d98da8d70756de19c9caeaf1fc
Justin Bogner <mail@justinbogner.com>
2010-05-20 19:16:11 -0600

    Graphical pomodoro timer

--- pomodoro.py
+++ pomodoro.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-import gtk, glib, subprocess, signal, sys
-from egg import trayicon
+import gtk, glib, os, subprocess, signal, sys
+
+DATA_DIRECTORY = os.path.join(os.path.dirname(__file__), 'data')
 
 def main(argv):
     indicator = Indicator('pomodoro')
+    indicator.run()
     try:
         gtk.main()
     except KeyboardInterrupt:
@@ -13,36 +15,91 @@ def main(argv):
 
 class Indicator:
     def __init__(self, name):
-        icon = trayicon.TrayIcon(name)
-        self.label = gtk.Label(' %s ' % name)
-        icon.add(self.label)
-        icon.show_all()
+        self.icon = gtk.StatusIcon()
+        work = IndicatorState('Work for 25 minutes', 'pomodoro.png', time = 25)
+        rest = IndicatorState('Take a short break', 'sleep.png', time = 5)
+        work.next = rest
+        rest.next = work
+        self.state = work
 
-        self.remaining = 0
-        self.doing = 'Rest'
+    def run(self):
+        self.transition()
+        self.update_gui()
+        glib.timeout_add(60 * 1000, self.run)
 
-        self.next_state()
+    def transition(self):
+        self.state.remaining -= 1
+        if self.state.remaining <= 0:
+            self.state = self.state.next
+            if not self.state:
+                self.quit()
+            prompt(self.state.message)
+            self.state.remaining = self.state.time
 
-    def quit(self, *args):
-        print 'foo'
+    def update_gui(self):
+        renderer = Renderer.get_singleton()
+        image = renderer.draw_with_counter(self.state.image_file,
+                                           self.state.remaining)
+        self.icon.set_from_pixbuf(image)
 
-    def next_state(self):
-        print "changing state"
-        self.remaining -= 1
-        if self.remaining <= 0:
-            if self.doing == 'Work':
-                prompt('Take a short break')
-                self.doing = 'rest'
-                self.remaining = 5
-            else:
-                prompt('Work for 25 minutes.')
-                self.doing = 'Work'
-                self.remaining = 25
-        self.update_text(' %s: %dm ' % (self.doing, self.remaining))
-        glib.timeout_add(60 * 1000, self.next_state)
+    def quit(self):
+        gtk.main_quit()
 
-    def update_text(self, text):
-        self.label.set_text(text)
+class IndicatorState:
+    def __init__(self, message, image_file, time):
+        self.message = message
+        self.image_file = image_file
+        self.time = time
+        self.remaining = self.time + 1
+        self.next = None
+
+def singleton(wrapped):
+    @staticmethod
+    def get_singleton():
+        instance = wrapped()
+        wrapped.get_singleton = staticmethod(lambda: instance)
+        return instance
+    wrapped.get_singleton = get_singleton
+    return wrapped
+
+@singleton
+class Renderer(object):
+    def __init__(self):
+        self.image_root_path = DATA_DIRECTORY
+        self.image_cache = {}
+
+    def draw_with_counter(self, image_file, number):
+        canvas = self.get_image(image_file).copy()
+        if number <= 0:
+            return canvas
+
+        digits = [self.get_image('%s.png' % c) for c in str(number)]
+
+        canvas_height = canvas.get_height()
+        canvas_width = canvas.get_width()
+
+        number_height = max(map(lambda x: x.get_height(), digits))
+        number_width = sum(map(lambda x: x.get_width(), digits))
+        h_scale = (2.0 * canvas_height) / (3.0 * number_height)
+        w_scale = (2.0 * canvas_width) / (3.0 * number_width)
+        scale = min(h_scale, w_scale)
+
+        offset_x = (canvas_width - int(number_width * scale)) / 2
+        offset_y = (canvas_height - int(number_height * scale)) / 2
+        for digit in digits:
+            digit.composite(canvas, 0, 0, canvas_width, canvas_height,
+                            offset_x, offset_y, scale, scale,
+                            gtk.gdk.INTERP_BILINEAR, 255)
+            offset_x += digit.get_width() * scale
+        return canvas
+
+    def get_image(self, image_file):
+        if not os.path.isabs(image_file):
+            image_file = os.path.join(self.image_root_path, image_file)
+        if image_file not in self.image_cache:
+            image = gtk.gdk.pixbuf_new_from_file(image_file)
+            self.image_cache[image_file] = image
+        return self.image_cache[image_file]
 
 def prompt(text):
     subprocess.Popen(['zenity', '--info', '--text', text]).wait()
