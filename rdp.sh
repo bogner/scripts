2e02cf05a325b2af8d9267d92405c3dc064c36d5
Justin Bogner <mail@justinbogner.com>
2012-04-25 22:47:14 -0600

    Don't override key bindings in rdp

--- rdp.sh
+++ rdp.sh
@@ -3,4 +3,5 @@
 host=${1:-localhost}
 port=${2:-3389}
 res=$(xdpyinfo | awk '/dimensions/ {print $2}' | awk -Fx '{print $1 "x" $2-24}')
-rdesktop -E -g$res $host:$port
+
+rdesktop -K -E -g$res $host:$port
