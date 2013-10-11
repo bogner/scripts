63767f28b0f95bfbcff5ea86401639a4ee946dcb
Justin Bogner <mail@justinbogner.com>
2013-10-10 17:53:57 -0700

    highlight.sh: BSD compatibility

--- highlight.sh
+++ highlight.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+esc=$(echo -e '\033')
 color=31
 exp=''
 while [ $# -gt 0 ] && [ "$1" != "--" ]; do
-    exp="$exp;"'s/\('"$1"'\)/\o33[1;'"$color"'m\1\o33[0m/g'
+    exp="$exp;"'s/\('"$1"'\)/'"$esc"'[1;'"$color"'m\1'"$esc"'[0m/g'
     color=$(( color + 1 ))
     shift
 done
